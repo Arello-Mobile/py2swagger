96bdec3c7d4d4588777f38f15a7c2e2c86bf0f10
Konstantin Misyutin <ikeeip@arello-mobile.com>
2016-06-09 21:44:52 +0600

    [feature][WDT-000] update setup classifiers

--- setup.py
+++ setup.py
@@ -17,16 +17,19 @@ setup(
     test_suite='nose.collector',
     author='Arello Mobile',
     url='https://github.com/Arello-Mobile/py2swagger',
+    platforms='Posix; MacOS X; Windows',
     classifiers=[
+        'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
-        'Topic :: Internet :: WWW/HTTP',
-        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Documentation',
     ],
     entry_points={
         'console_scripts': [
